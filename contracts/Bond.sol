fabf7d3703bc9ac0e6f1c15da3a5a5934924848f
luckyrobot <97241616+luckyrobot@users.noreply.github.com>
2022-02-07 10:41:01 -0600

    cleanup coupons

--- contracts/Bond.sol
+++ contracts/Bond.sol
@@ -1,35 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Bond {
   address _tokenContract;
   address _recipient;
-  uint256 _coupon;
   uint256 _startDate;
-  uint256 _couponsIssued;
-  uint256 _couponsToIssue;
   uint256 _interval;
 
   constructor(
     address tokenContract_,
     address recipient_,
-    uint256 coupon_,
-    uint256 couponsToIssue_,
     uint256 interval_
   ) {
     _tokenContract = tokenContract_;
-    _coupon = coupon_;
     _startDate = block.timestamp;
-    _couponsIssued = 0;
-    _couponsToIssue = couponsToIssue_;
     _interval = interval_;
     _recipient = recipient_;
-  }
-
-  function coupon() external view returns (uint256) {
-    return _coupon;
   }
 
   function recipient() external view returns (address) {
@@ -40,57 +28,22 @@ contract Bond {
     return _startDate;
   }
 
-  function couponsIssued() external view returns (uint256) {
-    return _couponsIssued;
-  }
-
-  function couponsToIssue() external view returns (uint256) {
-    return _couponsToIssue;
-  }
-
   function withdraw() external returns (uint256) {
-    uint256 _couponsPotentiallyEarned = (block.timestamp - _startDate) /
-      _interval;
-    uint256 _couponsEarned = _couponsPotentiallyEarned > _couponsToIssue
-      ? _couponsToIssue
-      : _couponsPotentiallyEarned;
-    uint256 _couponsOwed = _couponsEarned - _couponsIssued;
-    if (IERC20(_tokenContract).transfer(_recipient, _coupon * _couponsOwed)) {
+    uint256 _owed = (block.timestamp - _startDate) / _interval;
+    if (IERC20(_tokenContract).transfer(_recipient, _owed)) {
       // happy path, transferred the owed balance to the recipient
-      _couponsIssued = _couponsEarned;
-      return _coupon * _couponsOwed;
+      return _owed;
     } else {
-      // sad path, don't have the funds to do so... transfer the number of coupons
-      // we do have enough funds for
-      uint256 _availableCoupons = IERC20(_tokenContract).balanceOf(
-        address(this)
-      ) / _coupon;
-      // make sure that we actually can transfer the tokens the token contract claims we own,
-      // and that we actually don't have enough for the happy path. either of these cases indicate
-      // an unhappy ERC20 token
+      uint256 _available = IERC20(_tokenContract).balanceOf(address(this));
       require(
-        _availableCoupons < _couponsOwed,
-        "I could not transfer these coupons even though I possess them. This probably indicates a buggy ERC20 contract."
+        _available < _owed,
+        "I could not transfer these tokens even though I possess them. This probably indicates a buggy ERC20 contract."
       );
       require(
-        IERC20(_tokenContract).transfer(
-          _recipient,
-          _availableCoupons * _coupon
-        ),
+        IERC20(_tokenContract).transfer(_recipient, _available * _coupon),
         "Could not even transfer the tokens that the contract owns. This probably indicates a buggy ERC20 contract."
       );
-      _couponsIssued = _couponsIssued + _availableCoupons;
-      return _coupon * _availableCoupons;
+      return _available;
     }
-  }
-
-  function withdrawalLimit() external view returns (uint256) {
-    uint256 _couponsPotentiallyEarned = (block.timestamp - _startDate) /
-      _interval;
-    uint256 _couponsEarned = _couponsPotentiallyEarned > _couponsToIssue
-      ? _couponsToIssue
-      : _couponsPotentiallyEarned;
-    uint256 _couponsOwed = _couponsEarned - _couponsIssued;
-    return _coupon * _couponsOwed;
   }
 }
