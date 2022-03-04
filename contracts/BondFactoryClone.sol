8adb0a1cd7afd5459f57ccbbebc81a8b4029a143
bookland <bookland.porter.finance>
2022-03-04 07:23:09 -0800

    add allow-list

--- contracts/BondFactoryClone.sol
+++ contracts/BondFactoryClone.sol
@@ -2,13 +2,36 @@
 pragma solidity 0.8.9;
 import "./SimpleBond.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract BondFactoryClone {
+contract BondFactoryClone is AccessControl {
     address public immutable tokenImplementation;
+
+    bytes32 public constant ISSUER = keccak256("ISSUER");
+
     event BondCreated(address newBond);
 
     constructor() {
         tokenImplementation = address(new SimpleBond());
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function setupIssuers(address[] memory issuers)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        for (uint256 i = 0; i < issuers.length; i++) {
+            _setupRole(ISSUER, issuers[i]);
+        }
+    }
+
+    function revokeIssuers(address[] memory issuers)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        for (uint256 i = 0; i < issuers.length; i++) {
+            revokeRole(ISSUER, issuers[i]);
+        }
     }
 
     function createBond(
@@ -21,7 +44,7 @@ contract BondFactoryClone {
         address _borrowingAddress,
         bool _isConvertible,
         uint256 _convertibilityRatio
-    ) external returns (address clone) {
+    ) external onlyRole(ISSUER) returns (address clone) {
         clone = Clones.clone(tokenImplementation);
         SimpleBond(clone).initialize(
             _owner,
