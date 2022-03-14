1bec4d93a082a13ed211ad0741a6c5758276252c
Kyle <kyletrusler@gmail.com>
2022-03-14 10:09:01 -0700

    rename shares -> bonds, remove unnecessray things

--- contracts/SimpleBond.sol
+++ contracts/SimpleBond.sol
@@ -95,20 +95,13 @@ contract SimpleBond is
     // modifiers
     error BondPastMaturity();
     error BondNotYetMatured();
-    error BondNotYetRepaid();
-    error BondNotYetRedeemed();
-    error BondNotDefaulted();
 
     // Initialization
     error InvalidMaturityDate();
     error BackingRatioLessThanConvertibilityRatio();
 
     // Minting
-    error InusfficientCollateralToCoverTokenSupply();
     error BondSupplyExceeded();
-
-    // Withdraw
-    error CollateralInContractInsufficientToCoverWithdraw();
 
     // Repayment
     error RepaymentMet();
@@ -136,22 +129,6 @@ contract SimpleBond is
         _;
     }
 
-    /// @dev used to ensure bond has been repaid in full
-    modifier repaid() {
-        if (!isRepaid) {
-            revert BondNotYetRepaid();
-        }
-        _;
-    }
-
-    /// @dev used to check if the bond has defaulted
-    modifier defaulted() {
-        if (block.timestamp < maturityDate && !isRepaid) {
-            revert BondNotDefaulted();
-        }
-        _;
-    }
-
     uint256 internal constant ONE = 1e18;
 
     /// @notice A date in the future set at bond creation at which the bond will mature.
@@ -164,9 +141,6 @@ contract SimpleBond is
 
     /// @notice this flag is set after the issuer has paid back the full amount of borrowing token needed to cover the outstanding bonds
     bool internal isRepaid;
-
-    /// @notice this flag is set upon mint to disallow subsequent minting
-    bool internal _isIssued;
 
     /// @notice the addresses of the ERC20 token backing the bond which can be converted into before maturity or, in the case of a default, redeemable for at maturity
     address public collateralToken;
@@ -266,19 +240,19 @@ contract SimpleBond is
     }
 
     /// @notice mints the amount of specified bonds by transferring in collateral
-    /// @dev CollateralDeposited + Mint events are both emitted. shares to mint is bounded by maxSupply
-    /// @param shares the amount of bonds to mint
-    function mint(uint256 shares)
+    /// @dev CollateralDeposited + Mint events are both emitted. bonds to mint is bounded by maxSupply
+    /// @param bonds the amount of bonds to mint
+    function mint(uint256 bonds)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
         nonReentrant
         notPastMaturity
     {
-        if (shares > maxSupply - totalSupply()) {
+        if (bonds > maxSupply - totalSupply()) {
             revert BondSupplyExceeded();
         }
 
-        uint256 collateralToDeposit = previewMint(shares);
+        uint256 collateralToDeposit = previewMint(bonds);
 
         if (collateralToDeposit == 0) {
             revert ZeroAmount();
@@ -299,28 +273,28 @@ contract SimpleBond is
             collateralDeposited
         );
 
-        _mint(_msgSender(), shares);
+        _mint(_msgSender(), bonds);
 
-        emit Mint(_msgSender(), shares);
+        emit Mint(_msgSender(), bonds);
     }
 
     /// @notice Bond holder can convert their bond to underlying collateral
     /// @notice The bond must be convertible and not past maturity
-    /// @param shares the number of bonds which will be burnt and converted into the collateral at the convertibility ratio
-    function convert(uint256 shares) external notPastMaturity nonReentrant {
-        uint256 collateralToSend = previewConvert(shares);
+    /// @param bonds the number of bonds which will be burnt and converted into the collateral at the convertibility ratio
+    function convert(uint256 bonds) external notPastMaturity nonReentrant {
+        uint256 collateralToSend = previewConvert(bonds);
         if (collateralToSend == 0) {
             revert ZeroAmount();
         }
 
-        burn(shares);
+        burn(bonds);
 
         // @audit-ok Reentrancy possibility: the bonds are already burnt - if there weren't enough bonds to burn, an error is thrown
         IERC20(collateralToken).safeTransfer(_msgSender(), collateralToSend);
 
         totalCollateral -= collateralToSend;
 
-        emit Converted(_msgSender(), collateralToken, shares, collateralToSend);
+        emit Converted(_msgSender(), collateralToken, bonds, collateralToSend);
     }
 
     /// @notice allows the issuer to repay the bond by depositing borrowing token
@@ -353,9 +327,9 @@ contract SimpleBond is
     }
 
     /// @notice this function burns bonds in return for the token borrowed against the bond
-    /// @param bondShares the amount of bonds to redeem and burn
-    function redeem(uint256 bondShares) external nonReentrant pastMaturity {
-        if (bondShares == 0) {
+    /// @param bonds the amount of bonds to redeem and burn
+    function redeem(uint256 bonds) external nonReentrant pastMaturity {
+        if (bonds == 0) {
             revert ZeroAmount();
         }
 
@@ -363,11 +337,11 @@ contract SimpleBond is
         (
             uint256 borrowingTokensToSend,
             uint256 collateralTokensToSend
-        ) = previewRedeem(bondShares);
+        ) = previewRedeem(bonds);
 
         totalCollateral -= collateralTokensToSend;
 
-        burn(bondShares);
+        burn(bonds);
 
         // @audit-ok reentrancy possibility: the bonds are burnt here already - if there weren't enough bonds to burn, an error is thrown
         if (borrowingTokensToSend > 0) {
@@ -387,7 +361,7 @@ contract SimpleBond is
             _msgSender(),
             borrowingToken,
             collateralToken,
-            bondShares,
+            bonds,
             borrowingTokensToSend,
             collateralTokensToSend
         );
@@ -432,12 +406,12 @@ contract SimpleBond is
         return balanceAfter - balanceBefore;
     }
 
-    function previewMint(uint256 shares) public view returns (uint256) {
-        return (shares * backingRatio) / ONE;
+    function previewMint(uint256 bonds) public view returns (uint256) {
+        return (bonds * backingRatio) / ONE;
     }
 
-    function previewConvert(uint256 shares) public view returns (uint256) {
-        return (shares * convertibilityRatio) / ONE;
+    function previewConvert(uint256 bonds) public view returns (uint256) {
+        return (bonds * convertibilityRatio) / ONE;
     }
 
     function previewWithdraw() public view returns (uint256) {
@@ -483,7 +457,7 @@ contract SimpleBond is
         return totalCollateral - totalRequiredCollateral;
     }
 
-    function previewRedeem(uint256 shares)
+    function previewRedeem(uint256 bonds)
         public
         view
         returns (uint256 borrowingTokenToSend, uint256 collateralTokenToSend)
@@ -492,15 +466,15 @@ contract SimpleBond is
             return (0, 0);
         }
         if (isRepaid) {
-            return (shares, 0);
+            return (bonds, 0);
         } else {
             uint256 borrowingTokensInContract = IERC20(borrowingToken)
                 .balanceOf(address(this));
 
             // A defaulted bond is redeemable for its portion of borrowing tokens posibly deposited and the portion of collateral in the contract.
             return (
-                ((shares * borrowingTokensInContract) / (totalSupply())),
-                ((shares * totalCollateral) / (totalSupply()))
+                ((bonds * borrowingTokensInContract) / (totalSupply())),
+                ((bonds * totalCollateral) / (totalSupply()))
             );
         }
     }
