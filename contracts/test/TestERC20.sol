99a25df0360e7bdb0921b94b244999b652c5448c
Kyle <kyletrusler@gmail.com>
2022-03-15 14:39:15 -0700

    format

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -4,19 +4,19 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
-  uint8 private _decimals = 18;
+    uint8 private _decimals = 18;
 
-  constructor(
-    string memory name,
-    string memory symbol,
-    uint256 _mintAmount,
-    uint8 decimals_
-  ) ERC20(name, symbol) {
-    _mint(msg.sender, _mintAmount);
-    _decimals = decimals_;
-  }
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 _mintAmount,
+        uint8 decimals_
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, _mintAmount);
+        _decimals = decimals_;
+    }
 
-  function decimals() public view override returns (uint8) {
-    return _decimals;
-  }
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
 }
