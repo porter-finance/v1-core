b8683e148452d8cf0fd9c84c83d79cd0eb61ac25
bookland <bookland.porter.finance>
2022-03-15 16:14:06 -0500

    mint bonds

--- test/integration-tests/bondflow.spec.ts
+++ test/integration-tests/bondflow.spec.ts
@@ -1,19 +1,28 @@
 import { ethers, network } from "hardhat";
-import { deployNATIVEandBORROW, createBond } from "../setup";
+import { SimpleBond } from "../../typechain";
+import { deployNATIVEandBORROW, createBond, mint, initiateAuction } from "../setup";
 const { RINKEBY_DEPLOYER_ADDRESS } = process.env;
 const rinkebyFactory = "0x69e892D6c419883BFa5Def1FeB01cdf71129573d";
 
-describe("Integration", () => {
+describe.only("Integration", () => {
+
   if (!RINKEBY_DEPLOYER_ADDRESS)
     throw "{RINKEBY_DEPLOYER_ADDRESS} env variable is required";
-
   it("creates erc20 tokens and bonds", async () => {
-    const { native, borrow } = await deployNATIVEandBORROW();
     await network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [RINKEBY_DEPLOYER_ADDRESS],
     });
     const signer = await ethers.getSigner(RINKEBY_DEPLOYER_ADDRESS);
-    await createBond(signer, native, borrow, rinkebyFactory);
+    const { native, borrow } = await deployNATIVEandBORROW(signer);
+
+    const bond = await createBond(signer, native, borrow, rinkebyFactory) as SimpleBond;
+
+    await mint(signer, native, bond.address)
+
+    // await initiateAuction(signer, bond, borrow)
   });
+
+
+
 });
--- test/setup.ts
+++ test/setup.ts
@@ -1,14 +1,15 @@
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { TestERC20, BondFactoryClone } from "../typechain";
+import { TestERC20, BondFactoryClone, SimpleBond } from "../typechain";
 import { getBondContract } from "./utilities";
+const easyAuction = require('../contracts/external/EasyAuction')
 
-export const deployNATIVEandBORROW = async () => {
+export const deployNATIVEandBORROW = async (owner: SignerWithAddress) => {
   const MockErc20Contract = await ethers.getContractFactory("TestERC20");
-  const native = (await MockErc20Contract.deploy(
+  const native = (await MockErc20Contract.connect(owner).deploy(
     "Native Token",
     "NATIVE",
-    ethers.utils.parseUnits("500"),
+    ethers.utils.parseUnits("50000000", 20),
     18
   )) as TestERC20;
 
@@ -65,8 +66,8 @@ export const createBond = async (
         bondSymbol,
         owner.address,
         maturityDate,
-        nativeToken.address,
         borrowToken.address,
+        nativeToken.address,
         collateralRatio,
         convertibilityRatio,
         maxSupply
@@ -75,3 +76,60 @@ export const createBond = async (
   return bond;
 };
 
+export const mint = async (owner: SignerWithAddress, nativeToken: TestERC20, bondAddress?: string,
+) => {
+  let bond;
+  if (bondAddress) {
+    bond = (await ethers.getContractAt(
+      "SimpleBond",
+      bondAddress
+    )) as SimpleBond;
+  } else {
+    const SimpleBond = await ethers.getContractFactory(
+      "SimpleBond"
+    );
+    bond = await SimpleBond.connect(owner).deploy();
+  }
+
+  const approveTx = await nativeToken
+    .connect(owner).approve(bond.address, ethers.constants.MaxUint256)
+  await approveTx.wait();
+
+  const mintRole = await bond.MINT_ROLE();
+  const grantRoleTx = await bond
+    .connect(owner)
+    .grantRole(mintRole, owner.address);
+  await grantRoleTx.wait();
+  const tomint = await bond.previewMint(ethers.utils.parseUnits("50000000", 18));
+  console.log({ tomint: tomint.toString() })
+  const t = await nativeToken.balanceOf(owner.address)
+  console.log({ t })
+  await bond.connect(owner).mint(ethers.utils.parseUnits("50000000", 18));
+
+}
+
+export const initiateAuction = async (owner: SignerWithAddress, bond: SimpleBond,
+  borrowToken: TestERC20,
+) => {
+  const gnosisAddress = "0xC5992c0e0A3267C7F75493D0F717201E26BE35f7"
+  const auction = (await ethers.getContractAt(
+    easyAuction.abi,
+    gnosisAddress
+  ));
+  const auctioningToken = bond.address
+  const biddingToken = borrowToken.address
+  const orderCancellationEndDate = 0
+  const auctionEndDate = 1640301771
+  const _auctionedSellAmount = await bond.balanceOf(owner.address)
+  const _minBuyAmount = 1000000000000000
+  const minimumBiddingAmountPerOrder = 1000000000000000
+  const minFundingThreshold = 0
+  const isAtomicClosureAllowed = false
+  const accessManagerContract = ""
+  const accessManagerContractData = ""
+  const approveTx = await bond
+    .connect(owner).approve(gnosisAddress, ethers.constants.MaxUint256)
+  await approveTx.wait();
+
+  auction.connect(owner).initateAuction(auctioningToken, biddingToken, orderCancellationEndDate, auctionEndDate, _auctionedSellAmount, _minBuyAmount, minimumBiddingAmountPerOrder, minFundingThreshold, isAtomicClosureAllowed, accessManagerContract, accessManagerContractData)
+}
